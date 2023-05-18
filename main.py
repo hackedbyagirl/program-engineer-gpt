6dad7c2cbb7e7abf0e61d38f3b117575c907ee85
Brette <56555806+hackedbyagirl@users.noreply.github.com>
2023-05-18 18:35:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,12 @@ import argparse
 import os
 import sys
 
-from dotenv import load_dotenv
+from index_repo import 
 
-load_dotenv()
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
 
 def main():
   repo_path = os.getcwd()
-  repo_name = os.path.basename(repo_path)
-    
 
 if __name__ == "__main__":
     main()
