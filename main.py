597af0162f308bde43ab90b02060a1f22ea282f6
Brette <56555806+hackedbyagirl@users.noreply.github.com>
2023-05-18 18:35:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ import argparse
 import os
 import sys
 
-from index_repo import 
-
-
+from index_repo import load_and_index_files
 
 def main():
   repo_path = os.getcwd()
+  load_and_index_files(repo_path)
 
 if __name__ == "__main__":
     main()
