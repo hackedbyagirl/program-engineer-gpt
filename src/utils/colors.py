ff73e7e1c0a1ebc631be8756523225acf9099b2a
Brette <56555806+hackedbyagirl@users.noreply.github.com>
2023-06-08 17:23:57 +0000

    fix

--- src/utils/colors.py
+++ src/utils/colors.py
@@ -1,83 +1,65 @@
 #!/usr/bin/env python3
 
+import re
 from colored import fg, attr, stylize
-
 
 class Color(object):
 
     ''' Helpers for printing to terminal '''
 
-    '''
-    Color Keys:
-    red = 1
-    orange = 214
-    yellow = 220
-    green = 2
-    blue = 4
-    cyan = 81
-    purple = 141
-    gray = 245
-    white = 15
-
-    Attributes keys:
-    0 = reset
-    1 = bold
-    2 = dim
-    21 = reset bold
-    22 = rest dim
-
-    Extra colors:
-    33 = dog blue
-    226 = bright yellow
-    212 = nice purple
-    171 = purple
-    141 = cool purple
-    '''
+    colors = {
+        'W' : 15,  # white (normal)
+        'R' : 1,
+        'G' : 2,
+        'O' : 214,
+        'B' : 4,
+        'P' : 141,
+        'C' : 81,
+        'GR': 245,
+        'Y' : 226
+    }
 
     @staticmethod
-    def set(text, color, attrib=None):
-        r = 1
-        orng = 214
-        y = 220
-        g = 2
-        b = 4
-        cy = 81
-        purp = 141
-        gr = 245
-        wh = 15
+    def print(text):
+        '''
+        Prints text using colored format on same line.
+        Example:
+            Color.print('{R}This text is red. {W}This text is white')
+        '''
+        output = Color.s(text)
+        print(output)
 
-        if color == 'r':
-            color_code = r
+    @staticmethod
+    def s(text):
+        base = text
+        output = ''
+        start = 0
 
-        if color == 'orng':
-            color_code = orng
+        match_pattern = r"\{([A-Z])\}"
+        matches = re.findall(match_pattern, text)
 
-        if color == 'y':
-            color_code = y
+        for match in matches:
+            color_key = match
+            color_code = Color.colors[color_key]
+            r_code = "{" + color_key + "}"
 
-        if color == 'g':
-            color_code = g
+            base = base.replace(r_code, '')
+            regex = re.search(match_pattern, base)
 
-        if color == 'b':
-            color_code = b
-
-        if color == 'cy':
-            color_code = cy
-
-        if color == 'purp':
-            color_code = purp
-
-        if color == 'gr':
-            color_code = gr
-
-        if color == 'wh':
-            color_code = wh
+            if regex:
+                end = regex.start()
+                substring = base[start:end]
+                start = end
+            else:
+                substring = base[start:]
 
 
-        if attrib is None:
-            output = stylize(text, fg(color_code))
-        else:
-            style = fg(color_code) + attr(attrib)
-            output = stylize(text, style)
+            c_string = Color.color(substring, color_code) + ' '
+            output = ''.join([output, c_string])
 
         return output
+
+    @staticmethod
+    def color(text, code):
+        colored_text = stylize(text, fg(code))
+        return colored_text
