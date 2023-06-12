4dbe79c31daaeddc0c8049e94c888c295fd5e92c
Brette Geary <>
2023-06-12 09:59:33 -0600

    reformat code

--- src/utils/colors.py
+++ src/utils/colors.py
@@ -3,36 +3,37 @@
 import re
 from colored import fg, attr, stylize
 
+
 class Color(object):
 
-    ''' Helpers for printing to terminal '''
+    """Helpers for printing to terminal"""
 
     colors = {
-        'W' : 15,  # white (normal)
-        'R' : 1,
-        'G' : 2,
-        'O' : 214,
-        'B' : 4,
-        'P' : 141,
-        'C' : 81,
-        'GR': 245,
-        'Y' : 226
+        "W": 15,  # white (normal)
+        "R": 1,
+        "G": 2,
+        "O": 214,
+        "B": 4,
+        "P": 141,
+        "C": 81,
+        "GR": 245,
+        "Y": 226,
     }
 
     @staticmethod
     def print(text):
-        '''
+        """
         Prints text using colored format on same line.
         Example:
             Color.print('{R}This text is red. {W}This text is white')
-        '''
+        """
         output = Color.s(text)
         print(output)
 
     @staticmethod
     def s(text):
         base = text
-        output = ''
+        output = ""
         start = 0
 
         match_pattern = r"\{([A-Z])\}"
@@ -43,7 +44,7 @@ class Color(object):
             color_code = Color.colors[color_key]
             r_code = "{" + color_key + "}"
 
-            base = base.replace(r_code, '')
+            base = base.replace(r_code, "")
             regex = re.search(match_pattern, base)
 
             if regex:
@@ -53,9 +54,8 @@ class Color(object):
             else:
                 substring = base[start:]
 
-
-            c_string = Color.color(substring, color_code) + ' '
-            output = ''.join([output, c_string])
+            c_string = Color.color(substring, color_code) + " "
+            output = "".join([output, c_string])
 
         return output
 
