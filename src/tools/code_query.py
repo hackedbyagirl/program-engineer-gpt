f3734b5b5c86f1c6eb9141f3cd7486b4d3de37ef
Brette <56555806+hackedbyagirl@users.noreply.github.com>
2023-06-12 14:16:15 -0400

    Update code_query.py

--- src/tools/code_query.py
+++ src/tools/code_query.py
@@ -5,30 +5,35 @@ from langchain.tools import BaseTool
 from typing import Optional, Type
 from langchain.callbacks.manager import AsyncCallbackManagerForToolRun, CallbackManagerForToolRun
 from pydantic import BaseModel, Field
+from .core.loader import CodeLoader
 
 # Define the input schema for the tool
-class RepoAnalysisInput(BaseModel):
+class CodeAnalysisInput(BaseModel):
     repo_url: Optional[str] = Field(None, description="URL of the repository to analyze")
     current_directory: Optional[bool] = Field(None, description="Analyze code from the current directory")
     repo_path: Optional[str] = Field(None, description="Path to the local repository to analyze")
 
-class RepoAnalyzerTool(BaseTool):
-    name = "RepoAnalyzer"
+class CodeAnalyzerTool(BaseTool):
+    name = "CodeAnalyzer"
     description = "Analyzes a code repository and provides insights"
-    args_schema: Type[BaseModel] = RepoAnalysisInput
+    args_schema: Type[BaseModel] = CodeAnalysisInput
 
     def _run(self, repo_url: Optional[str], current_directory: Optional[bool], repo_path: Optional[str], run_manager: Optional[CallbackManagerForToolRun] = None) -> str:
-        """Use the tool."""
-        # Here you would put the logic to analyze the repository
-        # For the sake of this example, we will just return a dummy string
+        """Use CodeAnalyzerTool."""
         if repo_url:
-            return f"Analysis of repository {repo_url} completed successfully."
+            loader = CodeLoader(repo_url)
+            code_chunks = loader.load_repository()
+            return f"Analysis of repository {repo_url} completed successfully. Loaded {len(code_chunks)} code chunks."
         elif current_directory:
-            return f"Analysis of current directory completed successfully."
+            loader = CodeLoader('.')
+            code_chunks = loader.load_repository()
+            return f"Analysis of current directory completed successfully. Loaded {len(code_chunks)} code chunks."
         elif repo_path:
-            return f"Analysis of repository at {repo_path} completed successfully."
+            loader = CodeLoader(repo_path)
+            code_chunks = loader.load_repository()
+            return f"Analysis of repository at {repo_path} completed successfully. Loaded {len(code_chunks)} code chunks."
         else:
-            return "No valid input provided."
+            return "No valid input provided."        
 
     async def _arun(self, repo_url: Optional[str], current_directory: Optional[bool], repo_path: Optional[str], run_manager: Optional[AsyncCallbackManagerForToolRun] = None) -> str:
         """Use the tool asynchronously."""
