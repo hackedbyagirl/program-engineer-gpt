486059b7f41ce78386c30f419d798ef833058df5
Brette Geary <>
2023-06-12 10:04:23 -0600

    reformat code

--- src/core/splitter.py
+++ src/core/splitter.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
 
 from langchain.text_splitter import CharacterTextSplitter
-#from ..config import Config
+
+# from ..config import Config
+
 
 class CodeSplitter:
     def __init__(self, code_files):
         self.code_files = code_files
-        self.chunk_size = 1000 #Config.chunk_size
-        self.chunk_overlap = 0 #Config.chunk_overlap
+        self.chunk_size = 1000  # Config.chunk_size
+        self.chunk_overlap = 0  # Config.chunk_overlap
 
     def split_code(self):
         """
         Split the loaded code files into chunks by function or class.
         """
-        text_splitter = CharacterTextSplitter(chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap)
+        text_splitter = CharacterTextSplitter(
+            chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap
+        )
         texts = text_splitter.split_documents(self.code_files)
 
         return texts
-
