2ca1d76910bd503fb3091586949a8ccfd58d6734
Brette (BNastee) <56555806+hackedbyagirl@users.noreply.github.com>
2023-06-27 20:44:05 +0000

    cleanup

--- programengineergpt/core/developer.py
+++ programengineergpt/core/developer.py
@@ -5,12 +5,6 @@ import os
 
 from programengineergpt.agents.actions import Actions
 from programengineergpt.agents.ai import AIAgent
-from programengineergpt.prompts.intro import INTRO_SYSTEM_PROMPT
-from programengineergpt.prompts.setup_project import PROJECT_SETUP
-from programengineergpt.prompts.project_reqs import PROJECT_REQS
-from programengineergpt.prompts.project_design import PROJECT_DESIGN
-from programengineergpt.prompts.code_design import CODE_DESIGN
-from programengineergpt.prompts.code_writer import CODE_WRITER
 from programengineergpt.utils.colors import Color
 from programengineergpt.utils.display import Display
 
@@ -29,6 +23,8 @@ class Developer:
 
         # Initialize an AI agent
         self.ai_agent = AIAgent()
+        self.actions = Actions(self.ai_agent)
+
         self.start()
 
     def start(self):
@@ -37,38 +33,44 @@ class Developer:
 
         # Initialize Actions class
         Color.print("\n{L}Analyzing your project...\n")
-        actions = Actions(self.ai_agent)
 
-        # Launch the AI agent and begin project requirements
+        reqs = self.process_project_requirements()
+        design = self.process_project_design(reqs)
+        code_structure = self.process_code_structure(design)
+        self.write_initial_code(code_structure)
+
+    def process_project_requirements(self):
+        from programengineergpt.prompts.project_reqs import PROJECT_REQS
+        
         system_prompt = PROJECT_REQS
         user_prompt = self.project_description
         messages = self.ai_agent.launch(system_prompt, user_prompt)
+        return self.actions.clarify(messages)
 
-        # Get user clarification response
-        messages = actions.clarify(messages)
-
-        # Get Project requirements
+    def process_project_design(self, requirements):
+        from programengineergpt.prompts.project_design import PROJECT_DESIGN
+        
         Display.clear_screen()
         system_prompt = PROJECT_DESIGN
-        messages = actions.gen_design(system_prompt, messages)
-        messages = actions.clarify(messages)
+        messages = self.actions.gen_design(system_prompt, requirements)
+        return self.actions.clarify(messages)
 
-        # Move to the code strucuture setup
+    def process_code_structure(self, design):
+        from programengineergpt.prompts.code_design import CODE_DESIGN
+        
         Display.clear_screen()
         system_prompt = CODE_DESIGN
-        messages = actions.gen_code_structure(system_prompt, messages)
+        return self.actions.gen_code_structure(system_prompt, design)
 
-        # Write Initial Code
-        #Display.clear_screen()
+    def write_initial_code(self, structure):
+        from programengineergpt.prompts.code_writer import CODE_WRITER
+        
         system_prompt = CODE_WRITER
-        messages = actions.gen_code_structure(system_prompt, messages)
+        return self.actions.gen_code_structure(system_prompt, structure)
+    
 
     def write_output(self):
         # Save the user project input into the project folder directory
         with open(os.path.join(self.project_folder, 'project_instructions.txt'), 'w') as f:
             f.write(self.project_description)
-
-    def first_code_generation_phase(self, messages):
-        # Implement the logic for the first code generation phase
-        pass
 
