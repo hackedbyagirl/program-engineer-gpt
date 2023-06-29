2779ce2bffcc51c6885e3dd077020d1d7c5e30b8
Brette (BNastee) <56555806+hackedbyagirl@users.noreply.github.com>
2023-06-29 20:16:45 +0000

    fix

--- programengineergpt/core/__init__.py
+++ programengineergpt/core/__init__.py
@@ -1,6 +1,5 @@
 from programengineergpt.core.developer import Developer
-from programengineergpt.core.embedder import CodeEmbedder
 from programengineergpt.core.loader import CodeLoader
 from programengineergpt.core.retriever import Retriever
 
-__all__ = ["Developer", "CodeEmbedder", "CodeLoader", "Retriever"]
+__all__ = ["Developer", "CodeLoader", "Retriever"]
