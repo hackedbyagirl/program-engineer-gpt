89b515ed33f804dae0731abd692685084b369992
Brette (BNastee) <56555806+hackedbyagirl@users.noreply.github.com>
2023-06-28 21:50:05 -0400

    Update embedder.py

--- programengineergpt/core/embedder.py
+++ programengineergpt/core/embedder.py
@@ -1,36 +1,14 @@
 #!/usr/bin/env python3
 
 import os
-
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import DeepLake
-
 from programengineergpt.utils.colors import Color
 
 
 class CodeEmbedder:
-    def __init__(self, index_name, username):
-        self.index_name = index_name
-        self.username = username
-
-    def embed_and_upload(self, code_chunks):
-        """
-        Embed the code chunks using OpenAI Embeddings and Upload to DeepLake.
-        """
-        Color.print("{G}Step 4: {W}Embedding and Uploading files to Deeplake Index")
-        embeddings = OpenAIEmbeddings()
-        DeepLake.from_documents(
-            documents=code_chunks,
-            dataset_path=f"hub://{self.username}/{self.index_name}",
-            embedding=embeddings,
-            overwrite=True,
-            public=False,
-        )
-        self.remove_temp_dir()
-
-    def get_all_embeddings(self):
+    def __init__(self):
         pass
 
+    @staticmethod
     def remove_temp_dir(self):
         """
         Remove 'temp_repo' if a repository was cloned
